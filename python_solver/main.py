0e7bc595a0fc154b06a3d5d79d124a6c8e617b0e
Abhijit Gupta <avgupta456@gmail.com>
2022-12-09 01:42:51 -0500

    Benchmark Python
    About 3x slower than Rust (before optimizations)

--- python_solver/main.py
+++ python_solver/main.py
@@ -1,21 +1,37 @@
+from datetime import datetime, timedelta
+
 from algorithms.base import Algo
 from algorithms.random import RandomAlgo
 from algorithms.expectimax import ExpectimaxAlgo
 
-from game import get_initial_state, print_board, is_game_over, make_move
+from game import get_initial_state, get_score, is_game_over, make_move
 
 
-def simulate(algo: Algo):
+def run_game(algo: Algo):
+    moves = 0
     state = get_initial_state()
-    print_board(state)
-
     while not is_game_over(state):
         move = algo.get_move(state)
-        print('Move:', move)
         state = make_move(state, move)
-        print_board(state)
+        moves += 1
+    return get_score(state), moves
 
-    print('Game over!')
+def simulate(algo: Algo):
+    start = datetime.now()
+    time = 10
 
+    total_score = 0
+    total_games = 0
+    total_moves = 0
 
-simulate(ExpectimaxAlgo())
+    while datetime.now() - start < timedelta(seconds=time):
+        score, moves = run_game(algo)
+        total_score += score
+        total_moves += moves
+        total_games += 1
+
+    print("Games / Sec", total_games / time)
+    print("Moves / Sec", total_moves / time)
+    print("Score / Game", total_score / total_games)
+
+simulate(RandomAlgo())
